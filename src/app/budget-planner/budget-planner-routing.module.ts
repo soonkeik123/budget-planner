e61973c42231999bfae6b697e8b5eb47fdb13824
Nicky <soonkeik123@hotmail.com>
2024-06-19 15:59:50 +0800

    feat: profile page & form

--- src/app/budget-planner/budget-planner-routing.module.ts
+++ src/app/budget-planner/budget-planner-routing.module.ts
@@ -7,15 +7,17 @@ import { IncomeComponent } from './income/income.component';
 import { ExpenseComponent } from './expense/expense.component';
 import { TodoComponent } from './todo/todo.component';
 import { HistoryComponent } from './history/history.component';
+import { ProfileComponent } from './profile/profile.component';
 
 const routes: Routes = [
-  {path:'login', component: LoginComponent},
-  {path:'side-nav', component: SideNavComponent},
-  {path:'dashboard', component: DashboardComponent},
-  {path:'income', component: IncomeComponent},
-  {path:'expense', component: ExpenseComponent},
-  {path:'todo', component: TodoComponent},
-  {path:'history', component: HistoryComponent}
+  { path: 'login', component: LoginComponent },
+  { path: 'side-nav', component: SideNavComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'income', component: IncomeComponent },
+  { path: 'expense', component: ExpenseComponent },
+  { path: 'todo', component: TodoComponent },
+  { path: 'history', component: HistoryComponent },
+  { path: 'profile', component: ProfileComponent }
 ];
 
 @NgModule({
---
+++ src/app/budget-planner/profile/profile.component.ts
@@ -0,0 +1,43 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { SideNavComponent } from '../side-nav/side-nav.component';
+
+@Component({
+  selector: 'app-profile',
+  standalone: true,
+  imports: [ReactiveFormsModule, CommonModule, SideNavComponent],
+  templateUrl: './profile.component.html',
+  styleUrl: './profile.component.scss'
+})
+export class ProfileComponent {
+  profileForm: any;
+
+  constructor(private fb: FormBuilder, private snackBar: MatSnackBar) { }
+
+  ngOnInit(): void {
+    this.profileForm = this.fb.group({
+      name: ['', Validators.required],
+      age: ['', [Validators.required, Validators.min(18)]],
+      dob: ['', Validators.required],
+      gender: ['', Validators.required],
+      occupation: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      address: ['', Validators.required],
+      contact: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if (this.profileForm.valid) {
+      console.log("Form Saved: \n", this.profileForm.value);
+    }
+    else {
+      this.openSnackBar('Please fill in all fields correctly!', 'Close', 3000);
+    }
+  }
+  openSnackBar(message: string, action: string, duration: number) {
+    this.snackBar.open(message, action, { duration: duration });
+  }
+}
